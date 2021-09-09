be58c1dfb93a728a5152e40bc5b4c040f81da29e
lisab1993 <brown.lisa999@gmail.com>
2021-09-09 10:52:58 -0700

    deploy

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template
-  
+from flask import Flask
+
 app = Flask(__name__)
-  
+
 @app.route("/")
-def index():
-        return render_template('index.html')
+def home_view():
+		return "<h1>Welcome to Geeks for Geeks</h1>"
